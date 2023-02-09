54e7a8148f9a438b7561d12e384d78e126df72d9
safedhaouadi <safadhaouadi319@gmail.com>
2023-02-09 16:24:42 +0100

    update

--- test_Decision Trees/train.py
+++ test_Decision Trees/train.py
@@ -3,14 +3,14 @@ from sklearn.model_selection import train_test_split
 import numpy as np
 from DecisionTree import DecisionTree
 
-data = datasets.load_breast_cancer()
+data = datasets.load_iris()
 X, y = data.data, data.target
 
 X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.2, random_state=1234
+    X, y, test_size=0.3, random_state=1500
 )
 
-clf = DecisionTree(max_depth=10)
+clf = DecisionTree(max_depth=12)
 clf.fit(X_train, y_train)
 predictions = clf.predict(X_test)
 
